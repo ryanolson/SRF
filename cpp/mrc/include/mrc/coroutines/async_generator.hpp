afb561df5a743b7791d2b6da308b7f08e879565d
Ryan Olson <rolson@nvidia.com>
2023-01-11 03:47:13 +0000

    adding original copyright to async_generator.hpp

--- cpp/mrc/include/mrc/coroutines/async_generator.hpp
+++ cpp/mrc/include/mrc/coroutines/async_generator.hpp
@@ -15,6 +15,31 @@
  * limitations under the License.
  */
 
+/**
+ * Original Source: https://github.com/lewissbaker/cppcoro
+ * Original License: MIT; included below
+ */
+
+// Copyright 2017 Lewis Baker
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is furnished
+// to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #pragma once
 
 #include "mrc/utils/macros.hpp"
@@ -199,13 +224,13 @@ class AsyncGeneratorIterator final
     using handle_type  = std::coroutine_handle<promise_type>;
 
   public:
-    using iterator_category = std::input_iterator_tag;
+    using iterator_category = std::input_iterator_tag;  // NOLINT
     // Not sure what type should be used for difference_type as we don't
     // allow calculating difference between two iterators.
     using difference_type = std::ptrdiff_t;
     using value_type      = std::remove_reference_t<T>;
-    using reference       = std::add_lvalue_reference_t<T>;
-    using pointer         = std::add_pointer_t<value_type>;
+    using reference       = std::add_lvalue_reference_t<T>;  // NOLINT
+    using pointer         = std::add_pointer_t<value_type>;  // NOLINT
 
     AsyncGeneratorIterator(std::nullptr_t) noexcept : m_coroutine(nullptr) {}
 
